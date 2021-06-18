ef4468ab283a71d9c614e29cf7d95fd38bcbe534
fmribeiro <fmribeiro21@gmail.com>
2021-06-18 12:14:23 -0300

    subida com 76%

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,17 +1,14 @@
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
-import edu.princeton.cs.algs4.Stopwatch;
 
 public class PercolationStats {
 
-    private int n;
-    private int trials;
-    int[] grid;
-
+    private final int trials;
+    private final int[] grid;
 
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        if (n < 0 || trials < 0) {
+        if (n <= 0 || trials <= 0) {
             throw new IllegalArgumentException();
         }
 
@@ -19,8 +16,6 @@ public class PercolationStats {
         for (int i = 0; i < grid.length; i++) {
             grid[i] = i;
         }
-
-        this.n = n;
         this.trials = trials;
     }
 
@@ -36,32 +31,33 @@ public class PercolationStats {
 
     // low endpoint of 95% confidence interval
     public double confidenceLo() {
-        double zScore = 1.959964;
-        double standardError = stddev() / Math.sqrt(trials);
-        double marginError = standardError * zScore;
-        return mean() - marginError ;
+        return mean() - 1.96 * stddev() / Math.sqrt(trials);
     }
 
     // high endpoint of 95% confidence interval
     public double confidenceHi() {
-        double zScore = 1.959964;
-        double standardError = mean() / Math.sqrt(trials);
-        double marginError = standardError * zScore;
-        return mean() + marginError ;
+        return mean() + 1.96 * stddev() / Math.sqrt(trials);
     }
 
     // test client (see below)
     public static void main(String[] args) {
-        Stopwatch stopwatch = new Stopwatch();
-        int n = Integer.parseInt(args[0]);
-        int trials = Integer.parseInt(args[1]);
-        PercolationStats percolationStats = new PercolationStats(200, 100);
+//        int n = Integer.parseInt(args[0]);
+//        int trials = Integer.parseInt(args[1]);
+        int n = 1;
+        int trials = 100;
+        PercolationStats percolationStats = new PercolationStats(n, trials);
+        Percolation percolation = new Percolation(n);
+        for (int i = 0; i < trials; i++) {
+            int row = StdRandom.uniform(1, n + 1);
+            int col = StdRandom.uniform(1, n + 1);
+            percolation.open(row, col);
+        }
+
+//        double threshold = percolation.numberOfOpenSites() / (n * n);
+//        percolationThreshold[0] = threshold;
 
         System.out.println("mean: " + percolationStats.mean());
         System.out.println("stddev: " + percolationStats.stddev());
         System.out.println("95% confidence interval: [" + percolationStats.confidenceLo() + ", " + percolationStats.confidenceHi() + "]");
-
-        System.out.println((StdRandom.uniform(10)));
-        System.out.println(stopwatch.elapsedTime());
     }
 }
